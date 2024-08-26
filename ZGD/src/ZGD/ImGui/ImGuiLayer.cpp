0d106c8605738076fda33a6d6a7a824e6757d358
haofano <wd1786@163.com>
2024-08-26 17:48:41 +0800

    Update ImGui

--- ZGD/src/ZGD/ImGui/ImGuiLayer.cpp
+++ ZGD/src/ZGD/ImGui/ImGuiLayer.cpp
@@ -2,8 +2,8 @@
 #include "ImGuiLayer.h"
 
 #include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
+#include "examples/imgui_impl_glfw.h"
+#include "examples/imgui_impl_opengl3.h"
 
 #include "GLFW/glfw3.h"
 #include <glad/glad.h>
